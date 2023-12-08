266455156615b4e62f3faca4059d98ca1d23fc9b
Jawnathan <jonathan@jonathanschimpf.com>
2023-12-07 19:38:39 -0500

    update Header Quick Connect icons to include newly deployed .DEV

--- src/components/HeaderQuickConnects.js
+++ src/components/HeaderQuickConnects.js
@@ -6,6 +6,7 @@ import {
   faGithub,
   faInstagram,
   faThreads,
+  faDev,
 } from "@fortawesome/free-brands-svg-icons";
 // import { faFilePdf } from '@fortawesome/free-regular-svg-icons';
 import {  Container, Row, Col, Tooltip, OverlayTrigger } from "react-bootstrap";
@@ -15,6 +16,25 @@ function HeaderQuickConnects () {
     <>
       <Container className="d-flex justify-content-center">
         <Row className="icon-row">
+        <Col className="icon-col">
+          <OverlayTrigger
+              placement="bottom"
+              delay={{ show: 60, hide: 60 }}
+              overlay={<Tooltip className="tooltip-inner-quickconnects">.DEV</Tooltip>}
+            >
+            <div className="m-3 divquickconnects">
+              <a
+                href="https://jonathanschimpf.dev/"
+                rel="noreferrer"
+                target="_blank"
+                className="button responsiveIcon blackoutIcons"
+                aria-hidden="true"
+              >
+                <FontAwesomeIcon icon={faDev} size="2x" />
+              </a>
+            </div>
+            </OverlayTrigger>
+          </Col>
           <Col className="icon-col">
           <OverlayTrigger
               placement="bottom"
