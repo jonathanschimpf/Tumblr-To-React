bee9fae73256d35e0920d22866778b6c473d84e8
jonathanschimpf <jonathan@jonathanschimpf.com>
2025-06-14 19:55:17 -0400

    presss plz

--- src/components/HoverStrangeObservationsReveal.js
+++ src/components/HoverStrangeObservationsReveal.js
@@ -3,28 +3,31 @@ import "./HoverStrangeObservationsReveal.css";
 
 const HoverStrangeObservationsReveal = () => {
   const [hovered, setHovered] = useState(false);
+  const isTouchDevice =
+    typeof window !== "undefined" && "ontouchstart" in window;
 
-  const toggleHover = () => setHovered(!hovered);
-  const activateHover = () => setHovered(true);
-  const deactivateHover = () => setHovered(false);
+  const toggleHover = () => {
+    if (isTouchDevice) {
+      setHovered((prev) => !prev);
+    }
+  };
 
   return (
     <div
       className="hover-wrapper"
-      onMouseEnter={activateHover}
-      onMouseLeave={deactivateHover}
-      onTouchStart={toggleHover}
+      onMouseEnter={() => !isTouchDevice && setHovered(true)}
+      onMouseLeave={() => !isTouchDevice && setHovered(false)}
+      onClick={toggleHover} // SINGLE TAP = TOGGLE
     >
       <div className="hover-inner">
         <p className={`hover-name ${hovered ? "fade-out" : "fade-in"}`}>
           Jonathan Schimpf
         </p>
-
         <img
           src="/images/images-subdirectory_3/16174396485.jpg"
-          className={`hover-image ${hovered ? "fade-in" : "fade-out"}`}
           alt=""
           role="presentation"
+          className={`hover-image ${hovered ? "fade-in" : "fade-out"}`}
         />
       </div>
     </div>
